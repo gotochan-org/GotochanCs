993bb81a84bae384b215b8dca485655c96b2a247
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-06-15 18:10:42 +0100

    Refactor bundles

--- GotochanCs/Bundle.cs
+++ GotochanCs/Bundle.cs
@@ -1,9 +1,15 @@
 namespace GotochanCs;
 
 public abstract class Bundle {
-    public abstract string Name { get; }
-    public abstract Dictionary<string, Thingie> Options { get; }
-    public abstract Dictionary<string, Action<Actor>> ExternalLabels { get; }
+    public string Name { get; }
+    public Dictionary<string, Thingie> Options { get; }
+    public Dictionary<string, Action<Actor>> ExternalLabels { get; }
+
+    public Bundle() {
+        Name = GetName();
+        Options = [];
+        ExternalLabels = GetExternalLabels();
+    }
 
     public Thingie GetOption(string Key) {
         return Options[Key];
@@ -11,4 +17,13 @@ public abstract class Bundle {
     public void SetOption(string Key, Thingie Value) {
         Options[Key] = Value;
     }
+    public void IncludeBundle(Bundle Bundle) {
+        // Add external labels
+        foreach (KeyValuePair<string, Action<Actor>> ExternalLabel in Bundle.ExternalLabels) {
+            ExternalLabels[ExternalLabel.Key] = ExternalLabel.Value;
+        }
+    }
+
+    protected abstract string GetName();
+    protected abstract Dictionary<string, Action<Actor>> GetExternalLabels();
 }
